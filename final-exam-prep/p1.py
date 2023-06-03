919f797a35b010241b8be26c0764491ac5abd43b
oet247 <teodorzlatanov3v2@gmail.com>
2023-06-03 13:17:31 +0300

    p1 done

--- final-exam-prep/p1.py
+++ final-exam-prep/p1.py
@@ -1,29 +1,31 @@
 mes = input()
 comm = []
 def change (og: str, sub: str, rep: str):
-    l = [x for x in og]
-    og = l.join()
+    og = og.replace(sub, rep)
     return og
 def reverse (og: str, sub: str):
-    l = [x for x in og]
-    og = l.join()
-    return og
+    og = og.replace(sub, sub[::-1], 1)
+    return og     
 def space (og: str, index: int):
-    l = [x for x in og]
-    og = l.join()
+    l = [og[:index], ' ', og[index:]]
+    og = ''.join(l)
     return og
-while 1 == 1:
+for i in range(20):
     comm = input().split(":|:")
     if comm[0] == 'ChangeAll':
-        change(mes, comm[1], comm[2])
+        mes = change(mes, comm[1], comm[2]) 
         print(mes)
     if comm[0] == 'Reverse':
-        reverse(mes, comm[1])
-        print(mes)
+        if comm[1] in mes:
+            mes = reverse(mes, comm[1])
+            print(mes)
+        else:
+            print('error')
     if comm[0] == 'InsertSpace':
-        space(mes, comm[1])
+        mes = space(mes, int(comm[1]))
         print(mes)
     if comm[0] == 'Reveal':
-        print(f'You have a new text message: {mes}')    
+        print(f'You have a new text message: {mes}')
+        break    
     
     
